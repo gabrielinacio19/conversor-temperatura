fb6af0152b012b779e4a3d1cbad6ff5c997fca1a
gabrielcoatti <164410644+gabrielcoatti@users.noreply.github.com>
2026-07-15 22:55:49 +0000

    ✨ feat: adiciona conversão das unidades para Celsius

--- script.js
+++ script.js
@@ -3,3 +3,15 @@ const origem = document.getElementById("origem");
 const destino = document.getElementById("destino");
 const botao = document.getElementById("converter");
 const resultado = document.getElementById("resultado");
+
+function converterParaCelsius(temperatura, unidadeOrigem) {
+    if (unidadeOrigem === "fahrenheit") {
+        return (temperatura - 32) * 5 / 9;
+    }
+
+    if (unidadeOrigem === "kelvin") {
+        return temperatura - 273.15;
+    }
+
+    return temperatura;
+}
