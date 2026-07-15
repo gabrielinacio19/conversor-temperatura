145fe629fc816fe29fa79c30b093664c01206365
gabrielcoatti <164410644+gabrielcoatti@users.noreply.github.com>
2026-07-15 22:54:02 +0000

    feat: 🚀 adiciona referências aos elementos HTML

---
+++ script.js
@@ -0,0 +1,5 @@
+const valor = document.getElementById("valor");
+const origem = document.getElementById("origem");
+const destino = document.getElementById("destino");
+const botao = document.getElementById("converter");
+const resultado = document.getElementById("resultado");
